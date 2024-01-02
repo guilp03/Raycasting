8fc674ae7c96f453bdba1232141e998bef6ad519
Dhay <Diego50094@gmail.com>
2024-01-02 14:45:09 -0300

    Adicionada verificação de bounding box de objeto, antes dos triângulos
    Melhora a performance em cerca de 45%

--- camera.py
+++ camera.py
@@ -144,7 +144,7 @@ def intersect_triangle(ray, camera, triangulo_: triangulo.Triangulo):
 
 def collor(camera, vetor_atual, objetos):
     ''''' FUNÇÃO PARA CALCULAR A COR DE CADA OBJETO COM BASE NA MENOR DISTANCIA DA CAMERA '''''
-    T = INF
+    t = INF
     cor = np.array([30,30,30])
     for i in objetos:
         cor_t = i[1]
@@ -191,12 +191,26 @@ def collor(camera, vetor_atual, objetos):
             tmp = np.dot(i[5],vetor_atual)
             if tmp == 0:
                 continue
-            (current, cor_t)= intersect_triangle(vetor_atual, camera, i)
+            (current, cor_t) = intersect_triangle(vetor_atual, camera, i)
+        elif i[0] == "objeto":
+            '''Intersecção com objeto'''
+            # Verifica bouding box
+            x_max = i.x_max
+            x_min = i.x_min
+            y_max = i.y_max
+            y_min = i.y_min
+            z_max = i.z_max
+            z_min = i.z_min
+            if not test_bounding_box(camera, vetor_atual, x_max, x_min, y_max, y_min, z_max, z_min):
+                continue
+            
+            # Verifica intersecção com os subobjetos
+            (current, cor_t) = collor(camera, vetor_atual, i.subobjetos)
         # pega o menor tempo e joga em T
-        if current < T:
-            T = current
+        if current < t:
+            t = current
             cor = cor_t
-    return cor
+    return (t, cor)
 
 '''FUNÇÃO PARA RETIRAR O NP.ARRAY, FACILITANDO COMPOSIÇÃO DE TRANSFORMAÇÕES'''
 def nparray_para_tuple(vetor):
@@ -372,15 +386,17 @@ for triangulo_ in quadrado:
 
 
 cubo = obj.read_obj("cube2.obj", (50,140,70), texture_on=True)
+cubo_objeto = triangulo.Objeto()
 for triangulo_ in cubo:
-    adcionar_triangulo(*triangulo_, textura_quadrado)
+    cubo_objeto.adcionar_triangulo(*triangulo_, textura_quadrado)
+objetos.append(cubo_objeto)
     
 # for que percorre toda a tela e gera a intesecção com os objetos
 # para gerar a imagem final
 for i in range(hres):
     for j in range(vres):
         vetor_atual = vetor_inicial + i*desl_h + j*desl_v
-        imagem[j,i] = collor(camera, vetor_atual, objetos)
+        (_, imagem[j,i]) = collor(camera, vetor_atual, objetos)
     print(f"{'{:.2f}'.format(i*100/hres)}%", end='\r')
 print("100.00%", end='\r')
 cv.imshow("grupo06 - ANTES", imagem)
--- triangulo.py
+++ triangulo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import textura
-
+import triangulo
+INF = 9999999999
 def normalize(v):
     ''''' NORMALIZAÇÃO DE VETOR (NÃO TEM NO NUMPY) '''''
     v = v/np.linalg.norm(v)
@@ -65,3 +66,55 @@ class Triangulo:
                 return self.t3
             case 9:
                 return self.text
+            
+class Objeto:
+    subobjetos: list
+    x_max: float
+    x_min: float
+    y_max: float
+    y_min: float
+    z_max: float
+    z_min: float
+    def __init__(self) -> None:
+        self.subobjetos = []
+        self.x_max = -INF
+        self.x_min = INF
+        self.y_max = -INF
+        self.y_min = INF
+        self.z_max = -INF
+        self.z_min = INF
+        
+    def __getitem__(self, key):
+        ''' 0 = TIPO '''
+        match key:
+            case 0:
+                return "objeto"
+            case 1:
+                return np.array((0,0,0))
+            
+    def adcionar_triangulo(self, cor, p1, p2, p3, t1 = None, t2 = None, t3 = None, text: textura.Textura = None):
+        ''''' 0 = TIPO | 1 = COR | 2,3,4 = PONTO | 5 = VETOR_NORMAL | 6,7,8 = VETOR_TEXTURA | 9 = TEXTURA '''''
+        # transforma os pontos em array
+        p1 = np.array(p1)
+        p2 = np.array(p2)
+        p3 = np.array(p3)
+        
+        self.x_max = max(self.x_max, p1[0], p2[0], p3[0])
+        self.x_min = min(self.x_min, p1[0], p2[0], p3[0])
+        self.y_max = max(self.y_max, p1[1], p2[1], p3[1])
+        self.y_min = min(self.y_min, p1[1], p2[1], p3[1])
+        self.z_max = max(self.z_max, p1[2], p2[2], p3[2])
+        self.z_min = min(self.z_min, p1[2], p2[2], p3[2])
+        
+        if t1 and t2 and t3 and text:
+            t1 = np.array(t1)
+            t2 = np.array(t2)
+            t3 = np.array(t3)
+        else:
+            t1 = None
+            t2 = None
+            t3 = None
+            text = None
+        
+        #objetos.append(["triangulo", np.array(cor), p1, p2, p3, vetor, t1, t2, t3, text])
+        self.subobjetos.append(triangulo.Triangulo(np.array(cor),p1,p2,p3,t1,t2,t3,text))
