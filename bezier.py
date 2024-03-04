bdd5afa09a74d05f4f64424251417c79a119955e
Dhay <Diego50094@gmail.com>
2024-03-04 16:32:38 -0300

    Corrigida orientação dos triangulos na criação da malha da curva de bezier

--- bezier.py
+++ bezier.py
@@ -10,36 +10,28 @@ class BezierCurve:
     pontos_de_controle: List[List[np.ndarray]]
     
     @cache
-    def binomial(n, i):
-        return comb(n, i)
-    
-    @cache
-    def pow(u, i):
-        return u**i
-            
-    @cache
     def b(self, u, i, n):
         return comb(n, i)*pow(u,i)*pow(1-u, n-i)
     
     def B(self, u, v):
         res = 0
-        for i in range(0, self.n + 1):
-            for j in range(0, self.m + 1):
-                res += self.pontos_de_controle[i][j] * (self.b(u,i,self.n) * self.b(v,j,self.m))
+        for i in range(0, self.m + 1):
+            for j in range(0, self.n + 1):
+                res += self.pontos_de_controle[i][j] * (self.b(u,i,self.m) * self.b(v,j,self.n))
         return res
     
     
-    def __init__(self, n, m, *listas_de_pontos) -> None:
+    def __init__(self, m, n, *listas_de_pontos) -> None:
         self.pontos_de_controle = []
         self.n = n
         self.m = m
         for lista in listas_de_pontos:
-            assert lista.__len__() == m+1
+            assert lista.__len__() == n+1
             lista_ndarray = []
             for i in lista:
                 lista_ndarray.append(np.array(i))
             self.pontos_de_controle.append(lista_ndarray)  
-        assert self.pontos_de_controle.__len__() == n+1
+        assert self.pontos_de_controle.__len__() == m+1
         # print(self.pontos_de_controle)
         
     
@@ -81,7 +73,7 @@ class BezierCurve:
         # Ciano
         for i in range(0,resolucao-1):
             for j in range(1,resolucao):
-                objeto.adcionar_triangulo((0, 255, 255), pontos_render[i][j],pontos_render[i+1][j],pontos_render[i+1][j-1], _material = _material)
+                objeto.adcionar_triangulo((0, 255, 255), pontos_render[i][j], pontos_render[i+1][j-1], pontos_render[i+1][j], _material = _material)
         
         # TODO: diferentes alturas. Ou seja, mais camadas de bounding box e subobjetos
         # TODO: textura 
--- main.py
+++ main.py
@@ -39,157 +39,48 @@ def main():
     ''''' INICIALIZAÇÃO DOS OBJETOS PARA CASOS TESTE '''''
     
     # Adicionar luz
-    adicionar_luz(objeto.Luz((-4.0,3.0,0.0), (0.3,0.3,0.3)))
-    #adicionar_luz(objeto.Luz((0.0,3.0,5.0), (0.5,0.5,0.5)))
-    #adicionar_luz(objeto.Luz((0.0,3.0,-5.0), (0.5,0.5,0.5)))
+    adicionar_luz(objeto.Luz((-4.0,3.0,0.0), (0.5,0.5,0.5)))
+    adicionar_luz(objeto.Luz((0,-2,0),(0.5,0.5,0.5)))
     cena.COR_AMBIENTE = np.array((255,255,255))
 
-    # quadrado = readobj.read_obj("square.obj", (255,255,255))
-    # for triangulo_ in quadrado:
-    #     adcionar_triangulo(
-    #         *triangulo_, 
-    #         _material = objeto.Material(
-    #             kd=(0.0,0.0,0.0),
-    #             ke=(0.5,0.5,0.5),
-    #             ka=(0.1,0.1,0.1),
-    #             kr= 0.5,
-    #             kt = 0.5,
-    #             n = 1
-    #             )
-    #         )
-
-
-    #cubo = readobj.read_obj("cube.obj", (255,255,255))
-    #cubo_objeto = objeto.Objeto()
-    #for triangulo_ in cubo:
-    #    cubo_objeto.adcionar_triangulo(
-    #        *triangulo_,
-    #        _material = objeto.Material(
-    #            kd=(0.0,0.0,0.0),
-    #            ke=(0.5,0.5,0.5),
-    #            ka=(0.2,0.1,0.0),
-    #            kr=0.5,
-    #            kt = 0.5,
-    #            n = 3,
-    #            od = (160,70,0),
-    #            reflete=False
-    #        )
-    #    )
-    #OBJETOS_LISTA.append(cubo_objeto)
-    adcionar_plano((0,0,0), (0,5,0), (-1,-1,0), _material = objeto.Material(
-               kd=(0.4,0.4,0.4),
-               ke=(0.0,0.0,0.0),
-               ka=(0.0,0.0,0.0),
-               kr = 0.4,
-               kt = 0.9,
-               n = 2,
-               ior = 1.1,
-               od = (255,255,255),
-               reflete=False,
-               refrata=True))
-
-    #adcionar_esfera(0.4, (2,0,0), (255,255,255),
-    #        _material = objeto.Material(
-    #            kd=(0.0,0.0,0.0),
-    #            ke=(0.0,0.0,0.0),
-    #            ka=(0.0,0.0,0.0),
-    #            kr = 0.4,
-    #            kt = 0.9,
-    #            n = 2,
-    #            ior = 1.1,
-    #            od = (255,255,255),
-    #            reflete=False,
-    #            refrata=True
-    #        )
-    #)
-    #adcionar_esfera(0.4, (2,1,0), (255,255,255),
-    #       _material = objeto.Material(
-    #            kd=(0.0,0.0,0.0),
-    #            ke=(0.0,0.0,0.0),
-    #           ka=(0.0,0.0,0.0),
-    #            kr = 0.4,
-    #            kt = 0.9,
-    #            n = 2,
-    #            ior = 1.1,
-    #            od = (255,255,255),
-    #            reflete=False,
-    #            refrata=True
-    #        )
-    #)
-    #adcionar_esfera(0.4, (2,-1,0), (255,255,255),
-    #        _material = objeto.Material(
-    #            kd=(0.0,0.0,0.0),
-    #            ke=(0.0,0.0,0.0),
-    #            ka=(0.0,0.0,0.0),
-    #            kr = 0.4,
-    #            kt = 0.9,
-    #            n = 2,
-    #            ior = 1.1,
-    #            od = (255,255,255),
-    #            reflete=False,
-    #            refrata=True
-    #        )
-    #)
-
-    adicionar_luz(objeto.Luz((0,5,0),(0.5,0.5,0.5)))
-#     adcionar_esfera(1,(5,2,0), (255, 0, 0),
-#             objeto.Material(kd=(0.0,0.0,0.0),
-#             ke=(1,0,0.5),
-#             ka=(0.2,0.1,0.0),
-#             kr=0.5,
-#             kt=0.5,
-#             n=3,
-#             od=(255,0,127),
-#             reflete=False,
-#             refrata=False))
-    
-#     adcionar_esfera(1,(5,2,2), (255, 255, 0),
-#             objeto.Material(kd=(0.5,0.3,0.3),
-#             ke=(1,0,0.5),
-#             ka=(0.2,0.1,0.0),
-#             kr=0.5,
-#             kt=0.5,
-#             n=3,
-#             od=(255,0,127),
-#             reflete=False,
-#             refrata=False))
-    
-#     adcionar_esfera(1,(5,0,2), (0, 0, 255),
-#             objeto.Material(kd=(0.0,0.0,0.0),
-#             ke=(1,0,0.5),
-#             ka=(0.2,0.1,0.0),
-#             kr=0.5,
-#             kt=0.5,
-#             n=3,
-#             od=(255,0,127),
-#             reflete=False,
-#             refrata=False))
-    ## CURVA DE BEZIER
-    bez = bezier.BezierCurve(2, 2, [(7,0,-2),(7,1,-2),(7,2,-2)], [(7,1,-1),(7,2,-1),(7,3,-1)], [(7,2,2),(7,3,2),(7,4,3)])
-    malha = bez.criar_malha(resolucao=12, _material = objeto.Material(
-            kd=(0.0,0.0,0.0),
-            ke=(1,0,0.5),
-            ka=(0.2,0.1,0.0),
+ 
+    ## CURVAS DE BEZIER
+    bez = bezier.BezierCurve(3, 3, 
+                             [(7,0,-3.1),(7,1,-3),(7,2,-3),(7,3,-3)],
+                             [(7,0,-2),(8,1,-2),(8,2,-2), (7, 3, -2)], 
+                             [(7,0,-1),(8,1,-1),(8,2,-1), (7, 3, -1)], 
+                             [(7,0,0),(7,1,0),(7,2,0), (7,3,0)])
+    malha = bez.criar_malha(resolucao=15, _material = objeto.Material(
+            kd=(0.3, 0.0 ,0.15),
+            ke=(1.0, 0.0, 0.5),
+            ka=(0.2,0.0,0.1),
             kr=0.5,
             kt=0.5,
             n=3,
-            od=(255,0,127),
+            od=(255,255,255),
             reflete=False,
             refrata=False))
     cena.OBJETOS_LISTA.append(malha)
     
-    #adcionar_triangulo((255, 0, 127), (0,3,-3), (3,0,3), (0,3,0), 
-    #        objeto.Material(kd=(0.0,0.0,0.0),
-    #        ke=(0.5,0.5,0.5),
-    #        ka=(0.2,0.1,0.0),
-    #        kr=0.5,
-    #        kt = 0.5,
-    #        n = 3,
-    #        od = (160,70,0),
-    #        reflete=False,
-    #        refrata=True))
-    # for que percorre toda a tela e gera a intesecção com os objetos
-    # para gerar a imagem final
+    bez2 = bezier.BezierCurve(2, 2, 
+                             [(7,0,2),(7,1,2),(7,2,2)],
+                             [(7,0,3),(7,1,3),(7,2,3)], 
+                             [(7,0,4),(7,1,4),(7,2,4)])
+    
+    malha2 = bez2.criar_malha(resolucao=4, _material = objeto.Material(
+            kd=(0,0,0),
+            ke=(0.0, 1.0, 0.5),
+            ka=(0,0,0),
+            kr=0.5,
+            kt=0.5,
+            n=3,
+            od=(255,255,255),
+            reflete=False,
+            refrata=False))
+    cena.OBJETOS_LISTA.append(malha2)
+    
+    
+    
     for i in range(hres):
         for j in range(vres):
             vetor_atual = vetor_inicial + i*desl_h + j*desl_v
@@ -197,17 +88,7 @@ def main():
             print(f"{'{:.2f}'.format(i*100/hres)}%", end='\r')
     print("100.00%", end='\r')
 
-    cv.imshow("grupo06 - phong", imagem)
-    
-    # for i in range(hres):
-    #     for j in range(vres):
-    #         vetor_atual = vetor_inicial + i*desl_h + j*desl_v
-    #         imagem[j,i] = collor(camera, vetor_atual, OBJETOS_LISTA)[1]
-    #         print(f"{'{:.2f}'.format(i*100/hres)}%", end='\r')
-    # print("100.00%", end='\r')
-
-    # cv.imshow("grupo06 - collor", imagem)
-
+    cv.imshow("grupo06 - bezier", imagem)
     cv.waitKey(0)
     cv.destroyWindow('i')
 
